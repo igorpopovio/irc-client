40de1d6c99b3210c32888c23c53fc8e6162a5ef4
Igor Popov <igor@igorpopov.io>
2015-01-11 22:07:45 +0200

    Organize the code a bit
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- src/main/groovy/io/igorpopov/irc/MessageWriter.groovy
+++ src/main/groovy/io/igorpopov/irc/MessageWriter.groovy
@@ -2,17 +2,28 @@ package io.igorpopov.irc
 
 class MessageWriter implements Runnable {
     private Socket socket
-    private PrintStream stream;
+    private PrintStream stream
+    private Scanner scanner
 
     MessageWriter(Socket socket) {
         this.socket = socket
         this.stream = new PrintStream(socket.outputStream)
+        this.scanner = new Scanner(System.in)
     }
 
     @Override
     void run() {
-        def scanner = new Scanner(System.in)
+        setIdentity()
+        readMessagesFromKeyboardAndSendThemToTheServer()
+    }
+
+    def readMessagesFromKeyboardAndSendThemToTheServer() {
         while (scanner.hasNextLine())
             stream.println(scanner.nextLine())
+    }
+
+    def setIdentity() {
+        stream.println("NICK groovyIrcClient")
+        stream.println("USER groovyIrcClient 8 * :A Groovy Command Line Client")
     }
 }
