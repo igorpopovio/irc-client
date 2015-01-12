aa047731707a7a07366e4756d688c07ea3e8b9f2
Igor Popov <igor@igorpopov.io>
2015-01-12 20:54:33 +0200

    Reply with PONG when you receive a PING
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- src/main/groovy/io/igorpopov/irc/MessageReader.groovy
+++ src/main/groovy/io/igorpopov/irc/MessageReader.groovy
@@ -2,15 +2,28 @@ package io.igorpopov.irc
 
 class MessageReader implements Runnable {
     private Socket socket
+    private PrintStream stream
 
     MessageReader(Socket socket) {
         this.socket = socket
+        this.stream = new PrintStream(socket.outputStream)
     }
 
     @Override
     void run() {
         def scanner = new Scanner(socket.inputStream)
-        while (scanner.hasNext())
-            println scanner.nextLine()
+        while (scanner.hasNext()) {
+            def message = scanner.nextLine()
+            println message
+            keepConnectionAlive(message)
+        }
+    }
+
+    def keepConnectionAlive(String message) {
+        if (!message.startsWith("PING")) return
+
+        def pongReply = message.replace("PING", "PONG")
+        stream.println(pongReply)
+        println(pongReply)
     }
 }
