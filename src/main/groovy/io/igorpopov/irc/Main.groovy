95f0ec070191d915b054ee55e6a2d5a658f596ce
Igor Popov <igor@igorpopov.io>
2015-01-12 20:54:57 +0200

    Change the default server we connect to
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- src/main/groovy/io/igorpopov/irc/Main.groovy
+++ src/main/groovy/io/igorpopov/irc/Main.groovy
@@ -2,7 +2,7 @@ package io.igorpopov.irc
 
 class Main {
     public static void main(String[] args) {
-        def socket = new Socket("irc.freenode.org", 6667);
+        def socket = new Socket("irc.p2p-network.net", 6667);
         new Thread(new MessageReader(socket)).start();
         new Thread(new MessageWriter(socket)).start();
     }
